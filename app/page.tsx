27c389625d3eec0c4a54e790b11fb62f12dfa0b5
Vick Kumar <Vick.Kumar44@gmail.com>
2023-03-18 17:39:03 -0400

    added time and dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
 import Card from "@/components/card/Card";
-import { CalendarDaysIcon } from "@heroicons/react/24/solid";
+import {
+  ArrowPathIcon,
+  CalendarDaysIcon,
+  ClockIcon,
+} from "@heroicons/react/24/solid";
 export default function Home() {
   return (
     <div className="grid max-w-full grid-cols-3 gap-4 p-4">
       <div className="col-span-3">
-        <h4 className="text-7xl font-thin">
+        <h2 className="text-7xl font-thin">
           Welcome back, <span className="font-semibold">FirstHealth</span>
-        </h4>
+        </h2>
         <p className="mt-2 flex items-center">
           Last login <CalendarDaysIcon className="h-4 w-6" />
           <span className="font-semibold">{new Date().toDateString()}</span>
+        </p>
+      </div>
+      <div className="col-span-3 flex items-center">
+        <h4 className="text-2xl font-semibold">Dashboard</h4>
+        <button className="btn-ghost btn-circle btn">
+          <ArrowPathIcon className="h-6 w-6" />
+        </button>
+        <p className="mt-2 flex items-center">
+          updated as of <ClockIcon className="h-4 w-6" />
+          <span className="font-semibold">6 Hour(s) ago</span>
         </p>
       </div>
       <Card />
