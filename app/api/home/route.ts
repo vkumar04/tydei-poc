d506cfb24f779d72c644b6c51fb2207e90eca732
Vick Kumar <Vick.Kumar44@gmail.com>
2023-03-19 00:07:16 -0400

    commented out api

--- app/api/home/route.ts
+++ app/api/home/route.ts
@@ -1,6 +1,6 @@
-import { homepageData } from "@/app/consts/homepageData";
-import { NextRequest, NextResponse } from "next/server";
+// import { homepageData } from "@/app/consts/homepageData";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  return NextResponse.json({ data: homepageData });
-}
+// export async function GET(req: NextRequest) {
+//   return NextResponse.json({ data: homepageData });
+// }
