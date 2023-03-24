983f3190265dd6c11a114bb3d18c62f9e2bc68f7
Vick Kumar <Vick.Kumar44@gmail.com>
2023-03-24 09:43:02 -0400

    created top section

--- app/procedure/page.tsx
+++ app/procedure/page.tsx
@@ -1,7 +1,23 @@
+import {
+  DocumentArrowUpIcon,
+  RectangleGroupIcon,
+} from "@heroicons/react/24/solid";
+
 export default function Procedure() {
   return (
-    <div>
-      <h1>Procedure</h1>
+    <div className="grid min-w-full grid-cols-3 gap-4 p-4">
+      <div className="col-span-3 mt-6 flex items-center justify-between">
+        <p className="text-2xl font-semibold">Purchase Orders</p>
+        <div className="flex items-center space-x-4">
+          <button className="btn-circle btn">
+            <RectangleGroupIcon className="h-5 w-5" />
+          </button>
+          <button className="btn-circle btn">
+            <DocumentArrowUpIcon className="h-5 w-5" />
+          </button>
+          <button className="btn-primary btn">Create Purchase Order</button>
+        </div>
+      </div>
     </div>
   );
 }
